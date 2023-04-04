fde2d35548cb91318b556d1ef7294647e65da2d8
GordonSandercox <Gordon.Sandercox@gmail.com>
2023-04-04 14:55:40 -0500

    Added shopping page and dropdown.

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,10 +1,16 @@
 import './navigation.styles.scss';
 
-import { Fragment } from "react";
+import { Fragment, useContext } from "react";
 import { Outlet, Link } from "react-router-dom";
 import { ReactComponent as CrownLogo} from '../../assets/crown.svg'
+import { UserContext } from '../../contexts/user.context';
+import { signOutUser } from '../../utils/firebase/firebase.utils';
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 
 const Navigation = () => {
+    const {currentUser} = useContext(UserContext);
+
     return (
       <Fragment>
         <div className="navigation">
@@ -15,10 +21,14 @@ const Navigation = () => {
                 <Link className="nav-link" to='/shop'>
                     SHOP
                 </Link>
-                <Link className="nav-link" to='/auth'>
-                    SIGN IN
-                </Link>
+                {
+                    currentUser ? 
+                        <span className='nav-link' onClick={signOutUser}>SIGN OUT</span> :
+                        <Link className="nav-link" to='/auth'>SIGN IN</Link>
+                }
+                <CartIcon />
             </div>
+            <CartDropdown />
         </div>
         <Outlet />
       </Fragment>
